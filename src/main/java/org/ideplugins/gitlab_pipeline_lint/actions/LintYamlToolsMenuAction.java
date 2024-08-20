9e7b8d5b63a1c5fe0e63cd6d9d570fed750bbd59
Pablo Gutiérrez <2877137-pablomxnl@users.noreply.gitlab.com>
2024-08-20 15:25:34 +0200

    Fix #7

--- src/main/java/org/ideplugins/gitlab_pipeline_lint/actions/LintYamlToolsMenuAction.java
+++ src/main/java/org/ideplugins/gitlab_pipeline_lint/actions/LintYamlToolsMenuAction.java
@@ -2,6 +2,7 @@ package org.ideplugins.gitlab_pipeline_lint.actions;
 
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -31,24 +32,33 @@ public class LintYamlToolsMenuAction extends BaseAction {
         String gitlabCILintEndpoint = getGitlabUrl();
         if (checkGitlabToken() && !gitlabCILintEndpoint.contains("%") && event.getProject() != null) {
             Project project = getEventProject(event);
-            GlobalSearchScope scope = GlobalSearchScope.projectScope(Objects.requireNonNull(project));
-            List<VirtualFile> files = new ArrayList<>(FilenameIndex.getVirtualFilesByName(GITLAB_CI_YML, scope));
-            if (files.isEmpty()) {
-                displayNotification(NotificationType.WARNING, "No .gitlab-ci.yml file found.");
-                return;
-            }
-            if (files.size() > 1) {
-                displayNotification(NotificationType.WARNING,
-                        "Multiple .gitlab-ci.yml files found on project. Currently not supported");
-                return;
-            }
-            PsiManager psiManager = PsiManager.getInstance(project);
-            PsiFile psiFile = psiManager.findFile(files.get(0));
-            if (psiFile != null) {
-                doLintInBackground(event, psiFile);
+            DumbService dumbService = DumbService.getInstance(Objects.requireNonNull(project));
+            if (dumbService.isDumb()){
+                dumbService.runWhenSmart(() -> lintPipelineFile(project,event));
+            } else {
+                lintPipelineFile(project, event);
             }
         } else {
             displayNotificationWithAction(NotificationType.WARNING, "Please setup your Gitlab token/Project ID");
+        }
+    }
+
+    private void lintPipelineFile(@NotNull Project project, @NotNull AnActionEvent event) {
+        GlobalSearchScope scope = GlobalSearchScope.projectScope(project);
+        List<VirtualFile> files = new ArrayList<>(FilenameIndex.getVirtualFilesByName(GITLAB_CI_YML, scope));
+        if (files.isEmpty()) {
+            displayNotification(NotificationType.WARNING, "No .gitlab-ci.yml file found.");
+            return;
+        }
+        if (files.size() > 1) {
+            displayNotification(NotificationType.WARNING,
+                    "Multiple .gitlab-ci.yml files found on project. Currently not supported");
+            return;
+        }
+        PsiManager psiManager = PsiManager.getInstance(project);
+        PsiFile psiFile = psiManager.findFile(files.getFirst());
+        if (psiFile != null) {
+            doLintInBackground(event, psiFile);
         }
     }
 
